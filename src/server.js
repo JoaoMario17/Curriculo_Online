c205a719ee6f3014ee458955be0c7c7bd520dd27
João-Mário <joaomf17p2@gmail.com>
2021-05-07 19:35:24 -0300

    site proffy para teste

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
-app.use(express.static(path.join(__dirname,"..","pages")));
-app.set("views", path.join(__dirname,"..","pages"));
+app.use(express.static(path.join(__dirname,"..","public")));
+app.set("views", path.join(__dirname,"..","public"));
 app.engine("html",require("ejs").renderFile);
 app.set("view engine", "html");
 
 app.get('/', function (req,res) {
-    return res.render("index.html");
+    return res.render("pages/index.html");
 });
 
 
